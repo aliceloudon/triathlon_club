46cdfd75c009db1a08d1a26913af74958378d128
Alice Loudon <alice_loudon@icloud.com>
2017-05-15 09:28:32 +0100

    Sort CORS issue by installing rack-cors gem

--- triathlon_rails/config/routes.rb
+++ triathlon_rails/config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'members' => 'members#index'
-  get 'members/:id' => 'members#show'
-  post 'members' => 'members#create'
+  # get 'members' => 'members#index'
+  # get 'members/:id' => 'members#show'
+  # post 'members' => 'members#create'
 
-  get 'results' => 'results#index'
+  # get 'results' => 'results#index'
 
   # resources :members do
   #   resources :results
   # end
+
+  scope path: "api" do
+    resources :members, defaults: {format: :json}
+  end
 
 end
--- triathlon_react/client/src/containers/DashboardContainer.jsx
+++ triathlon_react/client/src/containers/DashboardContainer.jsx
@@ -6,8 +6,29 @@ class DashboardContainer extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      members: [{name:'Alice'}, {name:'Emily'}]
+      members: []
     }
+  }
+
+  componentDidMount(){
+    var url = 'http://localhost:5000/api/members'
+    var request = new XMLHttpRequest()
+    request.open('GET', url)
+
+    // request.setRequestHeader('Content-Type', "application/json")
+    // request.withCredentials = true
+
+    request.onload = () => {
+      if (request.status === 200){
+        // console.log("request: ", request.responseText)
+        var data = JSON.parse(request.responseText)
+        this.setState({members: data})
+      }
+      // else {
+      //   console.log("Error has occured.")
+      // }
+    }
+    request.send()
   }
 
   render(){
