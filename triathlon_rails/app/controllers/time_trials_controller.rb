45059944b1d47c6fbe9d087915c94fc29e7e6a84
Alice Loudon <alice_loudon@icloud.com>
2017-05-15 11:28:55 +0100

    Make create route for TimeTrial

--- triathlon_rails/app/controllers/time_trials_controller.rb
+++ triathlon_rails/app/controllers/time_trials_controller.rb
@@ -1,8 +1,17 @@
 class TimeTrialsController < ApplicationController
 
+  def time_trial_params
+    params.require(:time_trial).permit([:title, :date, :discipline, :distance])
+  end
+
   def index
-    timetrials = TimeTrial.all
-    render json: timetrials
+    time_trials = TimeTrial.all
+    render json: time_trials
+  end
+
+  def create
+    time_trial = TimeTrial.create(time_trial_params)
+    render json: time_trial
   end
 
 end
